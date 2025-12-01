b637fd09b7eee73138a76fb9596fc449ee4d3a98
Jesús Almanza <c.jesusalmanza@gmail.com>
2025-11-30 22:50:16 -0600

    First Commit

---
+++ NutriGestor/Domain/Utils/Validators/SesionValidator.swift
@@ -0,0 +1,7 @@
+//
+//  SesionValidator.swift
+//  NutriGestor
+//
+//  Created by José de Jesús Almanza Contreras on 24/11/25.
+//
+
---
+++ NutriGestor/Features/Patients/List/PatientsListViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  PatientsListViewModel.swift
+//  NutriGestor
+//
+//  Created by José de Jesús Almanza Contreras on 24/11/25.
+//
+
